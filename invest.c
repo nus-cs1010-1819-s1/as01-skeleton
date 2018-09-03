b961e57565b6d391df70a409e49eae4fe9245c55
Ooi Wei Tsang <weitsang@yahoo.com>
2018-09-03 09:09:33 +0000

    Fix formatting of comments.

--- invest.c
+++ invest.c
@@ -1,17 +1,15 @@
 /**
  * CS1010 Semester 1 AY18/19
  * Assignment 1: Invest
- * \file: invest.c
- * \author: XXX (Group YYY)
+ * Read the dollar amount of initial investment, the annual interest rate, 
+ * and the number of years k, and output the amount of money in the 
+ * investment after k years.
+ * @file: invest.c
+ * @author: XXX (Group YYY)
  */
 #include <math.h>
 #include "cs1010.h"
 
-/**
- * Read the dollar amount of initial investment, the annual interest rate, 
- * and the number of years k, and output the amount of money in the 
- * investment after k years.
- */
 int main() 
 {
 
--- taxi.c
+++ taxi.c
@@ -1,16 +1,14 @@
 /**
  * CS1010 Semester 1 AY18/19
  * Assignment 1: Taxi
- * \file: taxi.c
- * \author: XXX (Group YYY)
+ * Read the day, hour, and minute when a passenger board a taxi
+ * and the distance in meter travelled, and output the taxi fare.
+ * @file: taxi.c
+ * @author: XXX (Group YYY)
  */
 #include <stdbool.h>
 #include "cs1010.h"
 
-/**
- * Read the day, hour, and minute when a passenger board a taxi
- * and the distance in meter travelled, and output the taxi fare.
- */
 int main()
 {
 
